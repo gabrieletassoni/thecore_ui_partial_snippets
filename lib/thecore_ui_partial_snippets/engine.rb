28cdf48acba788ef5636e247f75879f7796a4445
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-01-31 22:41:24 +0100

    Added static assets manager

--- lib/thecore_ui_partial_snippets/engine.rb
+++ lib/thecore_ui_partial_snippets/engine.rb
@@ -1,4 +1,11 @@
 module ThecoreUiPartialSnippets
   class Engine < ::Rails::Engine
+    # This code makes the static assets present in this 
+    # engine available to main app, even if this gem is 
+    # just a dependency of another engine
+    initializer 'thecore_dataentry_commons.load_static_assets' do |app|
+      puts "Loading static assets for #{root}"
+      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+    end
   end
 end
