032a8933bba816b74f0cefbcdf58deb3b18a9547
Gabriele Tassoni <gabriele.tassoni@gmail.com>
2018-02-15 03:21:12 +0100

    readonly management for DateTimePicker

--- lib/thecore_ui_partial_snippets/engine.rb
+++ lib/thecore_ui_partial_snippets/engine.rb
@@ -3,9 +3,9 @@ module ThecoreUiPartialSnippets
     # This code makes the static assets present in this 
     # engine available to main app, even if this gem is 
     # just a dependency of another engine
-    initializer 'thecore_dataentry_commons.load_static_assets' do |app|
-      puts "Loading static assets for #{root}"
-      app.middleware.use ::ActionDispatch::Static, "#{root}/app"
-    end
+    # initializer 'thecore_dataentry_commons.load_static_assets' do |app|
+    #   puts "Loading static assets for #{root}"
+    #   app.middleware.use ::ActionDispatch::Static, "#{root}/app"
+    # end
   end
 end
--- lib/thecore_ui_partial_snippets/version.rb
+++ lib/thecore_ui_partial_snippets/version.rb
@@ -1,3 +1,3 @@
 module ThecoreUiPartialSnippets
-  VERSION = '1.1.3'
+  VERSION = '1.1.5'.freeze
 end
